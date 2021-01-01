d131ef44cf021f90a4b6ccfd7ba011aca1211884
Tania Campos <tania.santos36@yahoo.com.br>
2021-01-01 13:40:47 -0300

    GS-007: Set up the API

--- gsproduct/app/admin.py
+++ gsproduct/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
+    
+admin.site.register(Product, AppAdmin)
