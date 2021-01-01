19bbdf1343bcc4dd0c5b6daa3fc45f796d6c5bdc
Tania Campos <tania.santos36@yahoo.com.br>
2021-01-01 13:16:01 -0300

    GS-005: Creating Product model

--- gsproduct/app/models.py
+++ gsproduct/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+
+class Product(models.Model):
+    """
+        Product's model class
+    """
+
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+
+    def _str_(self):
+        return self.name
